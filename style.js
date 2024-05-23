30f140998b17ac170b0d4f34d58187a2ce2637c4
swathikagunasekaran <124919702+swathikagunasekaran@users.noreply.github.com>
2024-05-23 22:48:31 +0530

    Add files via upload

---
+++ style.js
@@ -0,0 +1,42 @@
+let menu = document.querySelector('#menu-bars');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () =>{
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+}
+
+window.onscroll= () =>{
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
+}
+
+function toggleDropdown() {
+    var dropdownContent = document.getElementById("myDropdown");
+    if (dropdownContent.style.display === "block") {
+        dropdownContent.style.display = "none";
+    } else {
+        dropdownContent.style.display = "block";
+    }
+}
+
+var swiper = new Swiper(".home-slider", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    coverflowEffect: {
+      rotate: 0,
+      stretch: 0,
+      depth: 100,
+      modifier: 2,
+      slideShadows: true,
+    },
+    loop :true,
+    autoplay:{
+        delay:1000,
+        disableOnInteraction:false,
+    }
+  });
+
+
